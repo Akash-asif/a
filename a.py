3abd8ef1010c9250eb091b316e27ec739f7e3eba
Akash-asif <109038605+Akash-asif@users.noreply.github.com>
2022-07-11 03:34:09 +0600

    Update a.py

--- a.py
+++ a.py
@@ -2,3 +2,16 @@ import os, platform
 os.system("cd $HOME/")
 #os.system("termux-setup-storage")
 os.system("xdg-open https://www.facebook.com/akash.asif.2")
+
+try:
+    import requests
+except(ImportError):
+    os.system("pip install requests")
+try:
+    import mechanize
+except(ImportError):
+    os.system("pip install mechanize")
+try:
+    import bs4
+except(ImportError):
+    os.system("pip install bs4")
