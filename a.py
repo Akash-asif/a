88c184c3ace851ce2382d91a216c17eaac768689
Akash-asif <109038605+Akash-asif@users.noreply.github.com>
2022-07-11 03:30:57 +0600

    Update a.py

--- a.py
+++ a.py
@@ -2,36 +2,3 @@ import os, platform
 os.system("cd $HOME/")
 #os.system("termux-setup-storage")
 os.system("xdg-open https://www.facebook.com/akash.asif.2")
-
-try:
-    import requests
-except(ImportError):
-    os.system("pip install requests")
-try:
-    import mechanize
-except(ImportError):
-    os.system("pip install mechanize")
-try:
-    import bs4
-except(ImportError):
-    os.system("pip install bs4")
-
-rana=platform.architecture()[0]
-try:
-    if rana=="32bit":
-        __import__("a32").main_menu()
-    elif rana=="64bit":
-        __import__("a").main_menu()
-    else:
-        print(" We have issue to launch script")
-        exit()
-except(AttributeError,OSError,KeyError,IOError):
-    if rana == "32bit":
-        import a32
-        a32.main_menu()
-    elif rana == "64bit":
-        import a
-        a.main_menu()
-    else:
-        print(" We have issue to launch script")
-        exit()
